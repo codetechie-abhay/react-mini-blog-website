3e44d983d8f9f0a8b2782df484521d80c8132655
Abhay Nautiyal 200203 <abhaynautiyal002@gmail.com>
2024-03-14 10:15:18 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer>
+      <div className="footer">
+        <ul>
+          <li>React JS</li>
+          <li>React Router</li>
+          <li>Draft JS</li>
+          <li>Love</li>
+          <li>By <a href="" rel="noopener noreferrer" target="_blank">Abhay nautiyal</a></li>
+        </ul>
+      </div>
+    </footer>
+  )
+}
